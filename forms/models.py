2f73927e31c05e2f703d48efc6bd848d817c20b7
Jhonattan Millán <millanjhonattan0@gmail.com>
2020-10-15 14:55:11 -0400

    Add StudentCard model

--- forms/models.py
+++ forms/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django_countries.fields import CountryField
 
-# Create your models here.
+class StudentCard(models.Model):
+	STUDIES_LIST = [
+		('PH', 'Physics'),
+		('CH', 'Chemistry'),
+		('ME', 'Medicine'),
+		('ML', 'Modern languages'),
+		('MS', 'Music'),
+		('VA', 'Visual arts'),
+		('GD', 'Graphic design'),
+		('MC', 'Mechanical engineering'),
+		('CO', 'COmputing')
+	]
+
+	first_name = models.CharField(max_length=65)
+	last_name = models.CharField(max_length=65)
+	birth_date = models.DateField(auto_now=False, auto_now_add=False)
+	country_of_origin = CountryField()
+	study = models.CharField(max_length=35,choices=STUDIES_LIST)
+	job = models.BooleanField()
+
+	def __str__(self):
+		return f'{self.first_name} {self.last_name}'
--- forms/tests.py
+++ forms/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from model_bakery import baker
+from forms.models import StudentCard
+
+import datetime
+
+class TestStudentCardModel(TestCase):
+	def setUp(self):
+		self.card1 = baker.make(StudentCard, first_name='Nick', last_name='Mason')
+		self.card2 = baker.make(StudentCard, first_name='Dave')
+
+	def test_created_properly(self):
+		cards = StudentCard.objects.all()
+
+		self.assertTrue(self.card1 in cards)
+		self.assertTrue(self.card2 in cards)
+
+		self.assertEqual(self.card1.first_name, 'Nick')
+		self.assertEqual(self.card2.first_name, 'Dave')
+
+	def test_str_method(self):
+		self.assertEqual(self.card1.__str__(), 'Nick Mason')
